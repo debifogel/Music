92f785d7eea4bedb32453ab567e614d94530d00f
debifogel <d0548431932@gmail.com>
2025-03-16 23:26:41 +0200

    after the kod remove

--- manager-client/src/app/app.component.spec.ts
+++ manager-client/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Manager' title`, () => {
+  it(`should have the 'manager-client' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Manager');
+    expect(app.title).toEqual('manager-client');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Manager');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, manager-client');
   });
 });
