e31b07ac1b9f66c228b96e5c4c939bbc9154083d
debifogel <d0548431932@gmail.com>
2025-05-19 17:45:03 +0300

    add more toast in addsong

--- Client/src/components/AddSong.tsx
+++ Client/src/components/AddSong.tsx
@@ -28,10 +28,19 @@ const AddSong = () => {
          {
            if (file) {
                await addSong( formData);
+               route('/songs/all')          
            } else {
                console.error("File is null");
-            route('/songs/all')          
-          }
+               toast.error("File is null", {
+                  position: "top-center",
+                  autoClose: 5000,
+                  hideProgressBar: false,
+                  closeOnClick: true,
+                  pauseOnHover: true,
+                  draggable: true,
+                  progress: undefined,
+               });
+             }
         } else {
             console.log("the client doesnt add file")    
        
