0486281280b36a6f74ffc641164d2c18b7534691
debifogel <d0548431932@gmail.com>
2025-05-19 17:44:33 +0300

    add cors

--- AIServer/main.py
+++ AIServer/main.py
@@ -167,13 +167,13 @@ def search_similar_songs(user_id, query_text, top_k=5):
     return results["matches"]
 
 app = FastAPI()
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["*"],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/process-audio/")
 async def process_audio(user_id: str, song_id: str, Audio: str):
